ca0cce43369331138a8c5414d8e24f8ac77592d4
CaoAnda <1401650042@qq.com>
2023-01-13 22:09:46 +0000

    反卷积+十分类

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ def get_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--lr', type=float, default=5e-5)
     parser.add_argument('--seed', type=int, default=3407)
     parser.add_argument('--decoding_method', type=str, default='deconv', choices=['upsample', 'deconv'])
-    parser.add_argument('--num_classes', type=int, default=2, choices=[2, 11])
+    parser.add_argument('--num_classes', type=int, default=11, choices=[2, 11])
     parser.add_argument('--output', action='store_true', default=True)  
   
     opt = parser.parse_args()  
