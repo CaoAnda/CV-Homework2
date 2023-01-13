868c9aca40bd466ed9a9ef095560ea33517a4014
CaoAnda <1401650042@qq.com>
2023-01-13 21:11:10 +0000

    增加十分类选择

--- net.py
+++ net.py
@@ -4,8 +4,10 @@ import  torch.nn.functional as F
 
 
 class LeNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_classes, decoding_method) -> None:
         super().__init__()
+        self.num_classes = num_classes
+        self.decoding_method = decoding_method
         # 3x28x28
         self.conv1 = nn.Sequential(     
             nn.Conv2d(3, 6, kernel_size=5, padding=2),  # 3x28x28
@@ -22,7 +24,7 @@ class LeNet(nn.Module):
         # 64x5x5
 
         # upsampling
-        self.upsamp = nn.Sequential(
+        self.upsample = nn.Sequential(
             nn.Upsample(scale_factor=2, mode='bilinear'),   # 64x10x10
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),  # 64x8x8
             nn.ReLU(),
@@ -30,7 +32,7 @@ class LeNet(nn.Module):
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),  # 64x14x14
             nn.ReLU(),
             nn.Upsample(scale_factor=2, mode='bilinear'),   # 64x28x28
-            nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1),
+            nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=1),
             nn.ReLU(),
         )
 
@@ -43,24 +45,24 @@ class LeNet(nn.Module):
             nn.ReLU(),
             nn.ConvTranspose2d(32, 32, kernel_size=5), # 32x29x29
             nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=2, kernel_size=2),
+            nn.Conv2d(in_channels=32, out_channels=num_classes, kernel_size=2),
             nn.ReLU(),
         )
-        # 2x28x28
+        # out_channelsx28x28
         
-
     def forward(self, x):
         out = self.conv1(x)
         out = self.conv2(out)
-        out = self.upsamp(out)
-        # out = self.deconv(out)
-
+        if self.decoding_method == 'upsample':
+            out = self.upsample(out)
+        elif self.decoding_method == 'deconv':
+            out = self.deconv(out)
         return out
 
 if __name__ == '__main__':
     input = torch.randn((1, 3, 28, 28))
     # print(input.numel())
-    model = LeNet()
+    model = LeNet(num_classes=2)
 
     out = model(input)
     print(out.shape)
