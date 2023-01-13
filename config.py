4a45b80ff58d927ab61574013d55e21af91ceeb2
CaoAnda <1401650042@qq.com>
2023-01-13 20:23:03 +0000

    upsample跑通

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--device', type=str, default='cuda:0')  
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--epochs', type=int, default=60)
-    parser.add_argument('--lr', type=float, default=1e-6)
+    parser.add_argument('--lr', type=float, default=5e-5)
     parser.add_argument('--seed', type=int, default=3407)
     # parser.add_argument('--input_size', type=int, default=32)
     parser.add_argument('--output', action='store_true', default=True)  
