eebec746bf19140e71e9c2b0a1393fe6f048f52c
CaoAnda <1401650042@qq.com>
2023-01-13 21:10:06 +0000

    添加可选参数

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import argparse
 def get_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--device', type=str, default='cuda:0')  
     parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--epochs', type=int, default=60)
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--lr', type=float, default=5e-5)
     parser.add_argument('--seed', type=int, default=3407)
-    # parser.add_argument('--input_size', type=int, default=32)
+    parser.add_argument('--decoding_method', type=str, default='upsample', choices=['upsample', 'deconv'])
+    parser.add_argument('--num_classes', type=int, default=2, choices=[2, 11])
     parser.add_argument('--output', action='store_true', default=True)  
   
     opt = parser.parse_args()  
